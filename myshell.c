e802ffc2048a0cc5718a2a847cdbb67d1858a6a9
Deliaml10 <114805861+Deliaml10@users.noreply.github.com>
2024-12-06 12:22:06 +0100

    jobs

--- myshell.c
+++ myshell.c
@@ -5,11 +5,129 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h> // Para open()
+#include <signal.h>
+
+void controlZ(){
+    printf("\nmsh> ");
+    fflush(stdout);
+}
+
+void controlC(){
+    printf("\nmsh> ");
+    fflush(stdout);
+}
+
+typedef struct job {
+    int id;  //numero del trabajo
+    pid_t pid;
+    char *command;
+    char *estado;
+}job;
+
+job *trabajos = NULL;  //inicializo la lista de trabajos a NULL
+int cuenta = 0;  //inicializo la cuenta de los trabajos a 0
+
+void addjob(pid_t pid, char *command, char *estado){
+    trabajos = realloc(trabajos, sizeof(struct job) * (cuenta + 1));
+    if(trabajos == NULL){
+        fprintf(stderr, "Error al asignar memoria para jobs\n");
+        exit(-1);
+    }
+    trabajos[cuenta].id = cuenta + 1;
+    trabajos[cuenta].pid = pid;
+    trabajos[cuenta].command = strdup(command); // se crea una copia de los comandos y se guarda en el tipo job
+    trabajos[cuenta].estado = strdup(estado); //duplica el valor del estado y lo guarda
+
+    if(trabajos[cuenta].command == NULL || trabajos[cuenta].estado == NULL){
+        fprintf(stderr, "Error al duplicar el mandato");
+        exit(-1);
+    }
+    cuenta++;
+}
+
+void showjob(){
+    for(int i = 0; i < cuenta; i++){
+        printf("[%d] %s %s\n", trabajos[i].id, trabajos[i].estado, trabajos[i].command);
+    }
+}
+
+void borrarjob(pid_t pid){
+    for(int i = 0; i < cuenta ; i++){
+        if(trabajos[i].pid == pid){
+            free(trabajos[i].command);
+            free(trabajos[i].estado);
+            for(int j = i; j < cuenta - 1; j++){
+                trabajos[j] = trabajos[j + 1]; // Corrección aquí, no "trabajos[j-1]"
+            }
+            cuenta--;
+
+            trabajos = realloc(trabajos, sizeof(struct job) * cuenta);
+            // Aquí se tiene en cuenta la variable "cuenta" porque si es igual a 0 el realloc daría NULL si o si, pero eso no sería un error
+            if(trabajos == NULL && cuenta > 0){
+                fprintf(stderr, "Error al reasignar memoria para los trabajos");
+                exit(-1);
+            }
+        }
+    }
+}
+
+void cambiarestado(pid_t pid, char *nuevoestado){
+    for(int i = 0; i < cuenta; i++){
+        if(trabajos[i].pid == pid){
+            free(trabajos[i].estado);
+            trabajos[i].estado = strdup(nuevoestado);
+            if(trabajos[i].estado == NULL){
+                fprintf(stderr, "Error al cambiar el estado del mandato");
+                exit(-1);
+            }
+        }
+    }
+}
+
+void borrartrabajos(){
+    for(int i = 0; i < cuenta; i++){
+        free(trabajos[i].estado);
+        free(trabajos[i].command);
+    }
+    free(trabajos);
+    trabajos = NULL;
+    cuenta = 0;
+}
+
+void trabajosterminados(){
+    int estado;
+    pid_t pid;
+
+    while((pid = waitpid(-1, &estado, WNOHANG)) > 0){
+        borrarjob(pid);
+    }
+}
+
+void moverforeground(pid_t pid){
+    for(int i = 0; i < cuenta; i++){
+        if(trabajos[i].pid == pid){
+            cambiarestado(pid, "Running");
+            int estado;
+            waitpid(pid, &estado, 0);
+            borrarjob(pid);
+        }
+    }
+}
 
 int main() {
     pid_t pid;
     char input[1024]; // Esto hay que hacerlo con malloc
     tline *line;
+
+    if(signal(SIGTSTP, controlZ) == SIG_ERR){
+        fprintf(stderr, "Error al configurar el Control+Z\n");
+        exit(-1);
+    }
+
+    if(signal(SIGINT, controlC) == SIG_ERR){
+        fprintf(stderr, "Error al configurar el Control+C\n");
+        exit(-1);
+    }
 
     while (1) { // Bucle principal del shell
         // Mostrar el prompt
@@ -24,60 +142,63 @@ int main() {
 
         line = tokenize(input); // Tokenizar la entrada
 
-	if(line == NULL || line->ncommands == 0){
-		continue;
-	}
+        if(line == NULL || line->ncommands == 0){
+            continue;
+        }
 
-	//comando cd
-	if(line->ncommands == 1 && strcmp(line->commands[0].argv[0], "cd") == 0){
-		char *nuevodir;
-		tcommand mandato = line->commands[0];
-		if(mandato.argc == 1){
-			nuevodir = getenv("HOME");
-			if(nuevodir == NULL){
-				fprintf(stderr, "El directorio no está definido\n");
-				continue;
-			}
-		}else{
-			nuevodir = mandato.argv[1];
-		}
+        //comando cd
+        if(line->ncommands == 1 && strcmp(line->commands[0].argv[0], "cd") == 0){
+            char *nuevodir;
+            tcommand mandato = line->commands[0];
+            if(mandato.argc == 1){
+                nuevodir = getenv("HOME");
+                if(nuevodir == NULL){
+                    fprintf(stderr, "El directorio no está definido\n");
+                    continue;
+                }
+            }else{
+                nuevodir = mandato.argv[1];
+            }
 
-		if(chdir(nuevodir) != 0){
-			fprintf(stderr, "Error al cambiar el directorio\n");
-			continue;
-		}else{
-			char diractual[1024];
-			if(getcwd(diractual, sizeof(diractual)) != NULL){
-				printf("El directorio actual es: %s\n", diractual);
-			}else{
-				fprintf(stderr, "Error al cambiar el directorio actual\n");
-				continue;
-			}
-		}
-	}
-
-
+            if(chdir(nuevodir) != 0){
+                fprintf(stderr, "Error al cambiar el directorio\n");
+                continue;
+            }else{
+                char diractual[1024];
+                if(getcwd(diractual, sizeof(diractual)) != NULL){
+                    printf("El directorio actual es: %s\n", diractual);
+                }else{
+                    fprintf(stderr, "Error al cambiar el directorio actual\n");
+                    continue;
+                }
+            }
+        }
+	// Mandato interno jobs
+        else if(line->ncommands == 1 && strcmp(line->commands[0].argv[0], "jobs") == 0){
+            	trabajosterminados();
+		showjob();
+        }
 
         // Salir si el usuario escribe "exit"
         else if (line->ncommands == 1 && strcmp(line->commands[0].argv[0], "exit") == 0) {
             printf("Se ha terminado el programa, hasta luego!\n");
             break;
-// BORRAR TODOS LOS MANDATOS "LIBERAR MEMORIA"
         }
+
         // Caso de un solo mandato
         else if (line->ncommands == 1) {
             tcommand mandato = line->commands[0];
 
-	    if(mandato.filename == NULL){
-		fprintf(stderr, "El mandato no existe");
-	    }
+            if(mandato.filename == NULL){
+                fprintf(stderr, "El mandato no existe");
+            }
 
             pid = fork();
             if (pid < 0) {
                 fprintf(stderr, "Error al crear el proceso hijo");
                 exit(-1);
             }
-	    // proceso hijo
+            // proceso hijo
             if (pid == 0) {
                 // Redirección de entrada
                 if (line->redirect_input != NULL) {
@@ -86,10 +207,9 @@ int main() {
                         fprintf(stderr, "Error al abrir el fichero de entrada");
                         exit(-1);
                     }
-		    printf("FICHERO ABIERTO");
+                    printf("FICHERO ABIERTO");
                     dup2(fichero, 0);
                     close(fichero);
-
                 }
 
                 // Redirección de salida
@@ -103,16 +223,16 @@ int main() {
                     close(fichero);
                 }
 
-		// Redireccion de salida error
-		if(line->redirect_error != NULL){
-			int fichero = open(line->redirect_error, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if(fichero < 0){
-				fprintf(stderr, "Error al abrir el fichero de salida error");
-				exit(-1);
-			}
-			dup2(fichero, 2);
-			close(fichero);
-		}
+                // Redireccion de salida error
+                if(line->redirect_error != NULL){
+                    int fichero = open(line->redirect_error, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                    if(fichero < 0){
+                        fprintf(stderr, "Error al abrir el fichero de salida error");
+                        exit(-1);
+                    }
+                    dup2(fichero, 2);
+                    close(fichero);
+                }
 
                 // Ejecutar el mandato
                 execvp(mandato.filename, mandato.argv);
@@ -121,11 +241,18 @@ int main() {
             } else {
                 // Proceso padre
                 int estado;
-                waitpid(pid, &estado, 0); // Esperar a que el hijo termine
+                if(line->background == 1){
+                    printf("El proceso con PID: %d, se está ejecutando en background\n", pid);
+                    addjob(pid, line->commands[0].argv[0], "Running"); // Cambié "line->commands[i]" por "line->commands[0].argv[0]"
+                    continue;
+                } else {
+                    waitpid(pid, &estado, 0); // Esperar a que el hijo termine
+                }
             }
         }
 
-        // Caso de mas de dos
+
+// Caso de mas de dos
         else if (line->ncommands >= 2) {
 		int numero_pipes = line->ncommands - 1;
 		int **tuberia = malloc(numero_pipes * sizeof(int *));
@@ -220,8 +347,16 @@ int main() {
             		close(tuberia[i][0]);
             		close(tuberia[i][1]);
 		}
-		for(int i = 0; i < line->ncommands; i++){
-            		wait(NULL); // Esperar al primer hijo
+		if(line->background == 1){
+			printf("El proceso con PID: %d, se está ejecutando en background\n", pid);
+			for(int i = 0; i < line->ncommands -1; i++){
+				addjob(pid, line->commands[i].argv[0], "Running");
+			}
+			continue;
+		}else{
+			for(int i = 0; i < line->ncommands; i++){
+            			wait(NULL); // Esperar al primer hijo
+			}
 		}
 	free(tuberia);
         } else {
